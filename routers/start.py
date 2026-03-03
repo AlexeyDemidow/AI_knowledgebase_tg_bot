6fcde80daff7d5d8d38bbaedc263b677b3eac954
AlexeyDemidow <usernzt@gmail.com>
2026-03-03 19:43:21 +0100

    Adding user to db when start bot

--- routers/start.py
+++ routers/start.py
@@ -4,6 +4,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from utils.states import BotStates
+from handlers.tasks_handler import create_user
 
 router = Router()
 
@@ -15,6 +16,9 @@ async def cmd_start(message: Message, state: FSMContext):
     )
     user_id = message.from_user.id
     user_name = message.from_user.username
+
+    await create_user(tg_id=str(user_id), username=user_name)
+
     await message.answer(text)
     await message.answer(f'Ваш ID: {str(user_id)}')
     await message.answer(f'Ваш юзернейм: {str(user_name)}')
