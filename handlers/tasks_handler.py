0f7619842038c370e6685d45928bf3c6a2addd66
AlexeyDemidow <usernzt@gmail.com>
2026-03-03 19:43:01 +0100

    Adding user to db when start bot using api

--- handlers/tasks_handler.py
+++ handlers/tasks_handler.py
@@ -5,5 +5,9 @@ from bot_settings import config
 
 async def create_user(tg_id: str, username: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get(config.url + '/user_create/' + f'?username={username}&tg_id={tg_id}') as response:
+        data = {
+            'tg_id': tg_id,
+            'username': username,
+        }
+        async with session.post(config.url + 'user_create/', json=data) as response:
             return await response.json()
