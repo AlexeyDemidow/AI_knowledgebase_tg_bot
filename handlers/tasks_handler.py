8a9c60a6fbc70f40f6203a705f8b3bc45abdfd69
AlexeyDemidow <usernzt@gmail.com>
2026-03-05 20:51:47 +0100

    Added connection to backend for bot responses

--- handlers/tasks_handler.py
+++ handlers/tasks_handler.py
@@ -11,3 +11,15 @@ async def create_user(tg_id: str, username: str):
         }
         async with session.post(config.url + 'user_create/', json=data) as response:
             return await response.json()
+
+
+async def ask_backend(tg_id, username, message):
+    payload = {
+        "tg_id": str(tg_id),
+        "username": username,
+        "message": message
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(config.url + 'chat/', json=payload) as resp:
+            return await resp.json()
