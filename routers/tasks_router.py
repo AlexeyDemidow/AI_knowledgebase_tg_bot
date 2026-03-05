6ecb2e76d5a62a959f58da33ce545f9d3b4b6eca
AlexeyDemidow <usernzt@gmail.com>
2026-03-05 20:51:01 +0100

    Added bot response to user messages

--- routers/tasks_router.py
+++ routers/tasks_router.py
@@ -1,61 +1,20 @@
 from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
-from aiogram.filters import Command
 
-# from handlers.tasks_handler import my_tasks, make_task_done
+from handlers.tasks_handler import ask_backend
 from utils.states import BotStates
 
 router = Router()
 
+@router.message()
+async def chat(message: Message, state: FSMContext):
 
-# @router.message(Command("tasks"))
-# async def user_tasks(message: Message):
-#     user_id = message.from_user.id
-#     tasks = await my_tasks(str(user_id))
-#
-#     if not tasks:
-#         await message.answer("Задачи не найдены")
-#
-#     result_text = "📌 Ваши задачи:\n\n"
-#
-#     for task in tasks:
-#         task_str = (
-#             f"🔢 ID: {task['id']}\n"
-#             f"📝 Название: {task['title']}\n"
-#             f"ℹ️ Описание: {task.get('description', 'нет описания')}\n"
-#             f"✅ Статус: {task['status']}\n"
-#             "——————\n"
-#         )
-#         result_text += task_str
-#
-#     await message.answer(result_text)
-#
-#
-# @router.message(Command("done"))
-# async def get_task_id(message: Message, state: FSMContext):
-#     await message.answer('Введите ID задачи.')
-#     await state.set_state(BotStates.waiting_for_task_id)
-#
-#
-# @router.message(BotStates.waiting_for_task_id)
-# async def change_status(message: Message, state: FSMContext):
-#     task_id = message.text.strip()
-#
-#     if not task_id.isdigit():
-#         await message.answer("❌ Неверный формат ID. Введите число.")
-#     else:
-#         result_text = "✅ Статус задачи изменен!\n\n"
-#
-#         task = await make_task_done(task_id)
-#
-#         task_str = (
-#             f"🔢 ID: {task['id']}\n"
-#             f"📝 Название: {task['title']}\n"
-#             f"ℹ️ Описание: {task.get('description', 'нет описания')}\n"
-#         )
-#
-#         result_text += task_str
-#         await message.answer(result_text)
-#
-#     await state.clear()
+    response = await ask_backend(
+        tg_id=message.from_user.id,
+        username=message.from_user.username,
+        message=message.text
+    )
+
+    await message.answer(response["answer"])
+    await state.set_state(BotStates.start)
