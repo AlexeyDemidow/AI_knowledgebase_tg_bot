e770a764393afa712fb549490a21eee4c98a7126
AlexeyDemidow <usernzt@gmail.com>
2026-03-19 19:17:41 +0100

    Added command "show_docs" handler

--- routers/start.py
+++ routers/start.py
@@ -5,7 +5,7 @@ from aiogram.types import Message
 
 from keyboards.mode_keyboard import mode_keyboard
 from utils.states import BotStates
-from handlers.tasks_handler import create_user
+from handlers.tasks_handler import create_user, show_docs
 
 router = Router()
 
@@ -40,6 +40,29 @@ async def cmd_add_doc(message: Message, state: FSMContext):
         "• DOCX\n"
         "• TXT"
     )
+
+    await message.answer(text)
+    await state.set_state(BotStates.add_document)
+
+
+@router.message(Command("show_docs"))
+async def cmd_add_doc(message: Message, state: FSMContext):
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+
+    response = await show_docs(
+        tg_id=str(user_id),
+        username=user_name
+    )
+
+    docs = response.get("docs", [])
+
+    if not docs:
+        text = "📄 У тебя пока нет документов"
+    else:
+        text = "📄 Твои документы:\n\n"
+        for doc in docs[:10]:
+            text += f"• {doc['name']}\n"
 
     await message.answer(text)
     await state.set_state(BotStates.add_document)
