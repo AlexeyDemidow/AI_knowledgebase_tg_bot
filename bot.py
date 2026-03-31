c83ff51e43bfa5bc9c1e8589a2b9e44b627d91c1
AlexeyDemidow <usernzt@gmail.com>
2026-03-31 20:02:38 +0200

    Update bot.py with all changes

--- bot.py
+++ bot.py
@@ -3,11 +3,10 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
-from handlers.signals import start_bot, stop_bot
-from utils import callbacks
+from routers import chat_handler, commands_handler, callbacks_handler
+from routers.signals_handler import start_bot, stop_bot
 from utils.commands import set_commands
 from bot_settings import config
-from routers import start, tasks_router
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value())
@@ -17,9 +16,9 @@ dp = Dispatcher()
 async def main():
     dp.startup.register(start_bot)
     dp.include_routers(
-        start.router,
-        tasks_router.router,
-        callbacks.router,
+        commands_handler.router,
+        chat_handler.router,
+        callbacks_handler.router,
     )
 
     dp.shutdown.register(stop_bot)
